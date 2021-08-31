4270979c0047a9b39a85f16da323fc1de7d3b270
experiencei <ayelojahighbee01@gmail.com>
2021-08-31 14:31:55 +0100

    working on the display of the score and ball

--- script.js
+++ script.js
@@ -195,9 +195,9 @@ function showGameOverEl(winner) {
 // Check If One Player Has Winning Score, If They Do, End Game
 function gameOver() {
   // if (playerScore === winningScore || computerScore === winningScore) {
-  //   isGameOver = ;
-  //   // Set Winner
-  //   let winner = ;
+    isGameOver = ;
+    // Set Winner
+    let winner = ;
   //   showGameOverEl(winner);
   // }
 }
